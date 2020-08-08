48bc95d4d8e3cd99caba7f65054b09ca78ead826
SHEKHAR SAXENA <shekhar.happy77@gmail.com>
2020-08-09 00:11:31 +0530

    Create Solution of Ques_03.cpp

---
+++ Solution of Ques_03.cpp
@@ -0,0 +1,41 @@
+/* Name : Shekhar Saxena
+  Class Roll No : 49
+  University Roll No : 2014852
+  Section : A 
+  
+  Cyber Security Assignment 01 : Question 03
+  Problem Statement : Write a C/C++ program that lists down all the prime numbers in a range between a and b, 
+  where a and b are two whole numbers.
+  -----------------------------------------------------------------------------------------------------*/
+
+#include <iostream>
+using namespace std;
+
+int is_prime(int x){
+    int flag = 1;
+    for(int i = 2; i <= x/2; i++)
+    {
+        if (x % i == 0)
+        {
+            flag = 0;
+            break;
+        }
+    }
+    return flag;
+}
+
+int main(){
+    int a, b;
+    cout << "Enter limits : ";
+    cin >> a >> b;
+    cout << "Prime Number between " << a << " and " << b << " are : ";
+
+    for(int i = a+1; i < b; i++){
+        int flag = is_prime(i);
+        if(flag = 1){
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+  }
