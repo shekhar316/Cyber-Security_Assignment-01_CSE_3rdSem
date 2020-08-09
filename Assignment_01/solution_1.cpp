9ad38c03103ec07cfe9f37bc89fc683db169ca5c
SHEKHAR SAXENA <shekhar.happy77@gmail.com>
2020-08-09 16:02:23 +0530

    Solution of Ques_01

--- Assignment_01/solution_1.cpp
+++ Assignment_01/solution_1.cpp
@@ -10,11 +10,19 @@
  
 #include <iostream>
 #include <cstdlib>
+#include <cctype>
 using namespace std;
 
 int main(int argc, char *argv[]){
     cout << "You have entered " << argc - 1 << " numbers.\n";
     int numbers[argc-1];
+  
+    for (int i = 1; i < argc ; i++){
+        if( !(isdigit(argv[i)) ){
+            cout << "Error : You have not entered a number. Please enter a diffrent number.";
+            exit(0);
+        }
+    }
 
     //converting string to numbers
     for (int i = 1; i < argc ; i++){
