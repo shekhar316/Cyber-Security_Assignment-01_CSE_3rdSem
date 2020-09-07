26521e0c18ae5bee7453a53360b0c9f2f23cd3b8
SHEKHAR SAXENA <shekhar.happy77@gmail.com>
2020-09-07 14:11:05 +0530

    Sum of three numbers

---
+++ Assignment_03/Solution06.py
@@ -0,0 +1,24 @@
+def blackjack(list):
+    sum = 0
+    for num in list:
+        sum = sum+num
+    if sum <= 21:
+        return sum
+    else:
+        flag = 0
+        for num in list:
+            if num == 11:
+                flag = 1
+                break
+        if (flag == 0):
+            return 'BUST'
+        else:
+            return sum
+
+
+list = []
+print("Input three numbers\n")
+for i in range(3):
+    x = int(input())
+    list.append(x)
+print(\nblackjack : ",blackjack(list))
